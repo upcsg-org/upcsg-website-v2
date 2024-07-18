7dc79114f9733cc0efe01c558db060230ffa9738
Maxell Milay <milaymaxell@gmail.com>
2024-07-18 20:26:28 +0800

    [Feature] setup some component template files

---
+++ components/educ-and-dev/AnnouncementCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AnnouncementCard = () => {
+    return <div>AnnouncementCard</div>
+}
+
+export default AnnouncementCard
---
+++ components/merch/MerchCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MerchCard = () => {
+    return <div>MerchCard</div>
+}
+
+export default MerchCard
