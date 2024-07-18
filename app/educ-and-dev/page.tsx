39caecc41449c0ed58c3571286fbf33c7d7be631
Maxell Milay <milaymaxell@gmail.com>
2024-07-18 20:06:55 +0800

    [Setup][Maxell] adde boilerplate folders and files

---
+++ app/educ-and-dev/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const EducAndDevPage = () => {
+    return <div>Educ and Dev Page</div>
+}
+
+export default EducAndDevPage
---
+++ app/merch/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MerchPage = () => {
+    return <div>Merch Page</div>
+}
+
+export default MerchPage
