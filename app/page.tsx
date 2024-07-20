f4740ed32ec6ae717b4f7c70b58b7f54d5da8d34
Maxell Milay <milaymaxell@gmail.com>
2024-07-20 19:27:26 +0800

    [Feature][Maxell] added text

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <main>
             <h1 className="font-vietnam text-6xl">homepage</h1>
+            <p>materiasl</p>
         </main>
     )
 }
