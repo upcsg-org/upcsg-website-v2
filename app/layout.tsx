1989a32afe314d48fcf49bd165c7d6b1bf36de47
Depyter <acabalharleyvan@gmail.com>
2024-07-22 16:36:47 +0800

    moved contact us form from root layout to homepage/landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import './globals.css'
 import TheHeader from '@/components/generics/TheHeader'
 import TheFooter from '@/components/generics/TheFooter'
-import ContactUsForm from '@/components/generics/ContactUsForm'
 
 export const metadata: Metadata = {
     title: 'UP Computer Science Guild',
@@ -19,7 +18,6 @@ export default function RootLayout({
             <body className="font-vietnam flex flex-col min-h-screen">
                 <TheHeader />
                 <main className="flex flex-col grow basis-0 ">{children}</main>
-                <ContactUsForm />
                 <TheFooter />
             </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import ContactUsForm from '@/components/generics/ContactUsForm'
+
 export default function Home() {
     return (
         <main>
             <h1 className="font-vietnam text-6xl">homepage</h1>
+            <ContactUsForm />
         </main>
     )
 }
