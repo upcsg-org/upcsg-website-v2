eeab3926d8a308174f60e215e2fd1df1b2d40594
Depyter <acabalharleyvan@gmail.com>
2024-07-22 21:26:54 +0800

    added contact us forms to educ-and-dev and merch

--- app/educ-and-dev/page.tsx
+++ app/educ-and-dev/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import ContactUsForm from '@/components/generics/ContactUsForm'
 
 const EducAndDevPage = () => {
-    return <div>Educ and Dev Page</div>
+    return <div>
+        Educ and Dev Page
+        <ContactUsForm />
+    </div>
 }
 
 export default EducAndDevPage
