d6fca9c53c3d12aad3df7edb01aa9ec2eed429e5
Maxell Milay <milaymaxell@gmail.com>
2024-07-23 20:39:48 +0800

    [Adjustment][Maxell] wrapped the contact us form in a section contianer

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import { Carousel } from '@/components/landing-page/Carousel'
 
 export default function Home() {
     return (
-        <section className="h-[calc(100vh-3rem)]">
-            <Carousel />
-            <ContactUsForm />
-        </section>
+        <>
+            <section className="h-[calc(100vh-3rem)]">
+                <Carousel />
+            </section>
+            <section>
+                <ContactUsForm />
+            </section>
+        </>
     )
 }
