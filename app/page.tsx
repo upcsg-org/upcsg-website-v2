77d9ed12f96fd6f46941493e3ec8e7c266dd427a
James Gabriel Elijah P. Ty <74401356+JGEPT@users.noreply.github.com>
2024-07-24 16:11:15 +0800

    [Feature][James] Created the upcoming events section, and also included the carousel into the edev page.

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import OfficersSection from '@/components/landing/OfficersSection'
 import ContactUsForm from '@/components/generics/ContactUsForm'
 import { Carousel } from '@/components/landing-page/Carousel'
 import FacultySection from '@/components/landing/FacultySection'
+import EventSection from '@/components/generics/EventSection'
 
 export default function Home() {
     return (
         <>
             <section className="h-[calc(100vh-3rem)]">
                 <Carousel />
+            </section>
+            <section>
+                <EventSection />
             </section>
             <section>
                 <OfficersSection />
---
+++ components/generics/EventCard.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { FaRegCalendarAlt } from 'react-icons/fa'
+
+interface EventCardProps {
+    image: string
+    title: string
+    date: string
+    time: string
+    dayOfWeek: string
+}
+
+const EventCard: React.FC<EventCardProps> = ({
+    image,
+    title,
+    date,
+    time,
+    dayOfWeek,
+}) => {
+    return (
+        <motion.button
+            className="relative overflow-hidden rounded-2xl w-full max-w-80 h-64 text-left bg-cover bg-center"
+            style={{
+                backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.3), rgba(0,0,0,0.4), rgba(65, 160, 30,0.5), rgba(65, 160, 30,1)), url('${image}')`,
+            }}
+            whileHover={{ scale: 1.05 }}
+            transition={{ duration: 0.3 }}
+        >
+            <div className="flex flex-col justify-between h-full p-4">
+                <div>
+                    <p className="text-lg font-vietnam font-extrabold text-white">
+                        {title}
+                    </p>
+                </div>
+                <div className="flex justify-between items-end">
+                    <div className="flex justify-between">
+                        <FaRegCalendarAlt className="h-4 w-4 mt-3" />
+                        <div className="pl-2">
+                            <p className="text-sm font-bold font-vietnam text-white">
+                                {date}
+                            </p>
+                            <p className="text-sm font-thin font-vietnam text-white">
+                                {time}
+                            </p>
+                        </div>
+                    </div>
+                    <p className="text-sm font-thin text-white mb-2">
+                        {dayOfWeek}
+                    </p>
+                </div>
+            </div>
+        </motion.button>
+    )
+}
+
+export default EventCard
