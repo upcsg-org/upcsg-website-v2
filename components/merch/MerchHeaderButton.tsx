07c35c30b1a4af8d57b03bd7a3cc8ac90fc28dcb
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2024-07-26 12:53:31 +0800

    [Enhancement][Joshua] Added hover effects to buttons

--- components/merch/MerchHeaderButton.tsx
+++ components/merch/MerchHeaderButton.tsx
@@ -16,7 +16,7 @@ const MerchHeaderButton = (props: PropsInterface) => {
 
     return (
         <div
-            className={`${styles} h-fill px-1 py-0.5 sm:px-2 sm:py-1 rounded-2xl flex flex-row gap-0.5 sm:gap-1 items-center`}
+            className={`${styles} h-fill px-1 py-0.5 sm:px-2 sm:py-1 rounded-2xl flex flex-row gap-0.5 sm:gap-1 items-center cursor-pointer hover:text-white/75 transition-colors`}
         >
             <Icon className="text-sm md:text-lg" />
             <span className="uppercase tracking-widest max-sm:hidden text-[10px] lg:text-[11px] xl:text-sm">
