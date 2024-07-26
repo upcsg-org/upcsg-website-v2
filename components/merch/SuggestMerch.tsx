7a4d89112e27b15487266fce839c63fcf45f4958
Depyter <acabalharleyvan@gmail.com>
2024-07-26 21:56:04 +0800

    fixed tshirt bg issue, redid colors

--- components/merch/SuggestMerch.tsx
+++ components/merch/SuggestMerch.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import SuggestMerchForm from './SuggestMerchForm';
+import Image from 'next/image';
 
 const SuggestMerch = () => {
 
@@ -13,19 +14,32 @@ const SuggestMerch = () => {
 
 
     return (
-        <div className='w-full h-fit'>
-            {showForm && <SuggestMerchForm handleClick={handleClick} />}
-            <div className='flex flex-col items-center h-fit'>
-                <h1 className='font-press-start text-2xl md:text-6xl text-white max-w-[20ch] text-center'>Have a tech-savvy design in mind?</h1>
-                <p className='font-vietnam text-lg md:text-3xl text-csg-gray max-w-[32ch] text-center mt-4'>Submit your binary brilliance and see it featured on our merch! Turn your computer science creativity into wearable art!</p>
-                <button
+    <div className='w-full h-fit bg-[#090C0F] pb-[50px] lg:pb-[300px] pt-10 md:pt-14'>
+        {showForm && <SuggestMerchForm handleClick={handleClick} />}
+        <div className='relative flex flex-col items-center h-fit z-10'>
+            <h1 className='font-press-start text-2xl md:text-6xl text-[#6E7E95] max-w-[20ch] text-center'>
+                Have a tech-savvy design in mind?
+            </h1>
+            <p className='font-vietnam text-lg md:text-3xl text-[#3B4247] max-w-[32ch] text-center mt-4'>
+                Submit your binary brilliance and see it featured on our merch! Turn your computer science creativity into wearable art!
+            </p>
+            <button
                 onClick={handleClick}
-                className='bg-main-dark text-white font-vietnam text-lg md:text-xl py-1 md:py-2 px-3 md:px-6 border-white border-2 mt-4 rounded-3xl'
-                >
+                className='bg-black text-[#5F6566] font-vietnam text-lg md:text-xl py-1 md:py-2 px-12 border-white border-2 mt-4 rounded-3xl'
+            >
                 SUBMIT HERE
-                </button>
-            </div>
+            </button>
         </div>
+        <div className='absolute bottom-[156px] lg:bottom-[300px] w-full z-1'>
+            <Image
+                src={'/images/suggest-merch-bg.png'}
+                alt='Suggest Merch'
+                height={1375}
+                width={785}
+                className='object-cover object-top w-full h-full md:max-h-[500px]'
+            />
+        </div>
+    </div>
     );
 };
 
