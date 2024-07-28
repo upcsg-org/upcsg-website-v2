fc457d0726b585225a3fcfe506d5447c3d2cf526
Maxell Milay <milaymaxell@gmail.com>
2024-07-28 14:16:59 +0800

    [Adjustment][Maxell] modified coming soon text

--- components/generics/ComingSoon.tsx
+++ components/generics/ComingSoon.tsx
@@ -4,9 +4,7 @@ const ComingSoon = () => {
     return (
         <div className="bg-main-dark w-full overflow-x-hidden h-[400px] flex items-center justify-center flex-wrap-reverse flex-row-reverse">
             <header className="text-5xl xs:text-7xl font-economica italic text-csg-green-100 font-bold text-center px-4">
-                THIS PAGE IS
-                <br />
-                UNDER CONSTRUCTION
+                FEATURE IN DEVELOPMENT
             </header>
         </div>
     )
