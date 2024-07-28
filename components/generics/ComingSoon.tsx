b6ebeaa1567c4b1a4559996ddcb478b4d727dc31
Maxell Milay <milaymaxell@gmail.com>
2024-07-28 17:33:26 +0800

    [Test][Maxell] test pipeline

--- components/generics/ComingSoon.tsx
+++ components/generics/ComingSoon.tsx
@@ -4,7 +4,7 @@ const ComingSoon = () => {
     return (
         <div className="bg-main-dark w-full overflow-x-hidden h-[400px] flex items-center justify-center flex-wrap-reverse flex-row-reverse">
             <header className="text-5xl xs:text-7xl font-economica italic text-csg-green-100 font-bold text-center px-4">
-                TESTING
+                TESTING PT.2
             </header>
         </div>
     )
