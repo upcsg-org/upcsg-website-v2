ab1cad77c3c5003923ff17e7bb7c51ce032fc08c
Maxell Milay <milaymaxell@gmail.com>
2024-08-10 10:51:22 +0800

    [Adjustment][Maxell] enlarged contact us page form

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import TextForm from '@/components/generics/ContactUsForm'
+'use client'
+
+import { useState } from 'react'
+import ContactUsForm from '@/components/generics/ContactUsForm'
+import ComingSoonModal from '@/components/generics/ComingSoonModal'
 
 const ContactUsPage = () => {
+    const [isContactUsModalOpen, setIsContactUsModalOpen] = useState(false)
+
     return (
-        <section>
-            <TextForm />
-        </section>
+        <div className="h-[calc(100vh-6rem)] flex flex-col justify-center">
+            <section>
+                <ContactUsForm
+                    setIsContactUsModalOpen={setIsContactUsModalOpen}
+                />
+            </section>
+            {isContactUsModalOpen && (
+                <ComingSoonModal toggleModal={setIsContactUsModalOpen} />
+            )}
+        </div>
     )
 }
 
