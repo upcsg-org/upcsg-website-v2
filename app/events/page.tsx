c562b8ef39053a4af00017d2f7cf2bb358ee0683
Maxell Milay <milaymaxell@gmail.com>
2024-08-10 10:47:54 +0800

    [Feature][Maxell] added events page

---
+++ app/events/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComingSoon from '@/components/generics/ComingSoon'
+
+const EventsPage = () => {
+    return (
+        <section className="h-[calc(100vh-6rem)]">
+            <ComingSoon />
+        </section>
+    )
+}
+
+export default EventsPage
