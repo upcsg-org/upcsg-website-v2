35d9f0f359921c3c42c09881f67d59595090884b
Maxell Milay <milaymaxell@gmail.com>
2024-08-10 10:45:46 +0800

    [Adjustment][Maxell] adjusted mock carousel data

--- constants/carousel.ts
+++ constants/carousel.ts
@@ -1,6 +1,6 @@
-export const LandingPageImages = {
+export const LandingPageCarouselContent = {
     First: {
-        image: '/carousel/slideshow-cover.png',
+        image: '/images/footer-bg.png',
         news: {
             title: 'PADAYON KOMSAI',
             description: 'The University of the Philippines CoThe University of the The University of the Philippines CoPhilippines CoThe University of the Philippines CoThe UniveThe University of the Philippines Corsity of the Philippines CoThe University of the Philippines Computer Science Guild is porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
@@ -8,16 +8,16 @@ export const LandingPageImages = {
         }
     },
     Second: {
-        image: '/images/footer-bg.png',
+        image: '/carousel/slideshow-cover.png',
         news: {
-            title: 'PADAYON KOMSAI 2',
-            description: 'KOMSAI KOMSAI KOMSAI',
-            link: '',
+            title: 'NEW UPCSG COUNCIL',
+            description: 'A diverse and dynamic community nurturing the next generation of trailblazing technology leaders',
+            link: 'https://www.facebook.com/UPCSG',
         }
     },
 }
 
-export const MerchLandingPageImages = {
+export const MerchPageCarouselContent = {
     First: {
         image: '/merch/merchlanding.png',
         news: {
