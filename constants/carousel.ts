a2dfaf94126b950c154ac835ecaabbf80c650a5a
Maxell Milay <milaymaxell@gmail.com>
2024-08-11 13:57:33 +0800

    [Adjustment][Maxell] updated carousel for merch page

--- constants/carousel.ts
+++ constants/carousel.ts
@@ -2,7 +2,7 @@ export const LandingPageCarouselContent = {
     First: {
         image: 'https://res.cloudinary.com/dc7anycov/image/upload/v1723343899/upcsg_aqmm8h.png',
         news: {
-            title: 'UP Computer Science Guild ',
+            title: 'UP Computer Science Guild',
             description: 'A diverse and dynamic community nurturing the next generation of trailblazing technology leaders',
             link: 'https://www.facebook.com/UPCSG',
         }
@@ -19,19 +19,17 @@ export const LandingPageCarouselContent = {
 
 export const MerchPageCarouselContent = {
     First: {
-        image: 'https://res.cloudinary.com/dc7anycov/image/upload/v1723343899/upcsg_aqmm8h.png',
+        image: 'https://res.cloudinary.com/dc7anycov/image/upload/v1723354995/merch-all_ryvnz3.png',
         news: {
-            title: 'UP Computer Science Guild ',
-            description: 'A diverse and dynamic community nurturing the next generation of trailblazing technology leaders',
-            link: 'https://www.facebook.com/UPCSG',
+            title: 'UPCSG Merchandise',
+            description: 'Elevate your style with a variety of items from caps to drawstring bags, stickers, pins, keychains, and lanyards! 👕🕶️',
         }
     },
     Second: {
-        image: 'https://res.cloudinary.com/dc7anycov/image/upload/v1723292944/Cespa_for_upcsg_website_ikmujk.png',
+        image: 'https://res.cloudinary.com/dc7anycov/image/upload/v1723355058/merch-package_uukfat.png',
         news: {
-            title: 'CESPA',
-            description: 'A collective network for inter-university cooperation within Cebu programmer students',
-            link: 'https://www.facebook.com/profile.php?id=61563488252275',
+            title: 'Merchandise Packages',
+            description: 'Power up bundles are available for those who want to get the most out of their purchase. ⭐️',
         }
     },
 }
