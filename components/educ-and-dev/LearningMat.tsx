3b25f2560675e13f175a69d34df32179566aca64
James Gabriel Elijah P. Ty <74401356+JGEPT@users.noreply.github.com>
2024-08-20 14:05:38 +0800

    [Feature][James] Finalized Learning Material Card Layout

--- components/educ-and-dev/LearningMat.tsx
+++ components/educ-and-dev/LearningMat.tsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import { PiFilePdfDuotone } from 'react-icons/pi'
+import { BiSolidFilePdf } from 'react-icons/bi'
 import { FiDownload } from 'react-icons/fi'
 
-const PDFPreviewCard = () => {
+const LearningMat = () => {
     return (
         <motion.div
-            className="bg-[#E2574C] overflow-hidden w-64 h-80 rounded-3xl text-center text-white flex flex-col"
+            className="bg-[#E2574C] overflow-hidden w-64 h-[360px] rounded-3xl text-center text-white flex flex-col"
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.3 }}
         >
-            <h2 className="text-lg font-semibold mt-4 mb-2">Lecture Notes</h2>
-            <div className="flex-grow flex flex-col items-center justify-center space-y-2">
-                <PiFilePdfDuotone className="text-white text-6xl" />
-                <p className="text-sm">PDF file</p>
-                <p className="text-base font-semibold">yearIV_primer.pdf</p>
+            <h2 className="text-xl font-extralight mt-2 mb-2 font-vietnam">
+                Lecture Notes
+            </h2>
+            <div className="flex-grow flex flex-col items-center justify-center space-y-1">
+                <BiSolidFilePdf className="text-white text-7xl" />
+                <p className="text-lg font-vietnam font-light">PDF file</p>
+                <p className="text-xl font-bold font-vietnam">
+                    yearIV_primer.pdf
+                </p>
             </div>
-            <div className="mb-4 space-y-2">
-                <button className="bg-[#E2574C] text-white py-1 px-4 text-sm underline">
+            <div className="mb-3">
+                <button className="bg-[#E2574C] text-white py-1 px-4 text-lg font-semibold font-vietnam underline">
                     Preview
                 </button>
-                <button className="bg-[#B53629] text-white py-2 px-4 text-sm flex items-center justify-center mx-auto w-full h-full">
-                    <span className="flex items-center justify-center pb-2 text-xl font-vietnam font-bold">
+                <button className="bg-[#B53629] text-white py-2 px-4 text-sm flex justify-center  relative top-2 mx-auto w-full h-full">
+                    <span className="flex items-center justify-center pt-1 text-xl font-vietnam font-semibold">
                         Download
                         <FiDownload className="ml-3 text-xl font-bold" />
                     </span>
@@ -31,4 +35,4 @@ const PDFPreviewCard = () => {
     )
 }
 
-export default PDFPreviewCard
+export default LearningMat
