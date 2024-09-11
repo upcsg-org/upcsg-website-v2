d65105c382e4b060873d6c02e20807373644e714
Maxell Milay <milaymaxell@gmail.com>
2024-09-11 10:03:51 +0800

    [Adjustment][Maxell] added newlines at EOF

--- components/merch/modals/ShoppingCartItem.tsx
+++ components/merch/modals/ShoppingCartItem.tsx
@@ -1,8 +1,8 @@
 import React, { useState, Dispatch, SetStateAction } from 'react'
 import Image from 'next/image'
-import { BsXLg } from "react-icons/bs"
+import { BsXLg } from 'react-icons/bs'
 import { MerchItem, MerchSize, MerchType } from '@/interface/merch'
-import { Color } from "@/interface/generic"
+import { Color } from '@/interface/generic'
 
 interface ShoppingCartItem {
     id: number
@@ -12,47 +12,60 @@ interface ShoppingCartItem {
     color: Color
     maxQuantity: number
     isCheckedOut: boolean
-    onCheckoutChange: (key:number, isCheckedOut: boolean) => void
+    onCheckoutChange: (key: number, isCheckedOut: boolean) => void
 }
 
 const ShoppingCartItem = (props: ShoppingCartItem) => {
     const minQuantity = 0
 
-    const { id, merch, quantity, size, color, maxQuantity, isCheckedOut, onCheckoutChange } = props
+    const {
+        id,
+        merch,
+        quantity,
+        size,
+        color,
+        maxQuantity,
+        isCheckedOut,
+        onCheckoutChange,
+    } = props
     const { name, type, price, images, sizes, colors, isBestSeller } = merch
 
     const [Quantity, setQuantity] = useState(quantity)
 
     const [Size, setSize] = useState(() => {
-        return sizes.find(sizeObj => sizeObj === size) ? size : sizes[0]
+        return sizes.find((sizeObj) => sizeObj === size) ? size : sizes[0]
     })
     const [IsCheckedOut, setIsCheckedOut] = useState(isCheckedOut)
 
     const [Color, setColor] = useState(() => {
-        return colors.find(colorObj => colorObj === color) ? color : colors[0]
+        return colors.find((colorObj) => colorObj === color) ? color : colors[0]
     })
 
     const handleToggle = () => {
-        setIsCheckedOut(prev => !prev)
+        setIsCheckedOut((prev) => !prev)
         const newCheckedOut = !IsCheckedOut
         console.log('pressed', newCheckedOut)
         onCheckoutChange(id, newCheckedOut)
     }
 
     const handleDecrement = () => {
-        setQuantity((prevQuantity) => Math.max(minQuantity, prevQuantity - 1));
-    };
+        setQuantity((prevQuantity) => Math.max(minQuantity, prevQuantity - 1))
+    }
 
     const handleIncrement = () => {
-        setQuantity((prevQuantity) => Math.min(maxQuantity, prevQuantity + 1));
-    };
+        setQuantity((prevQuantity) => Math.min(maxQuantity, prevQuantity + 1))
+    }
 
     return (
         // whole
         <div className="min-w-4 flex flex-row items-center gap-1 ps:gap-2 md:gap-4 overflow-hidden">
             {/*button*/}
-            <div onClick={handleToggle} className={`w-[16px] ps:w-[24px] md:w-[35px] aspect-square rounded-full border-2 md:border-4 border-[#171745] flex shrink-0 cursor-pointer items-center justify-center ${IsCheckedOut ? 'bg-[#2929b7]' : 'bg-[#2929b7]/0'
-                }`} />
+            <div
+                onClick={handleToggle}
+                className={`w-[16px] ps:w-[24px] md:w-[35px] aspect-square rounded-full border-2 md:border-4 border-[#171745] flex shrink-0 cursor-pointer items-center justify-center ${
+                    IsCheckedOut ? 'bg-[#2929b7]' : 'bg-[#2929b7]/0'
+                }`}
+            />
             {/*outline*/}
             <div className="min-w-4 flex flex-row gap-4 md:gap-8 border rounded-lg p-3 items-left w-full mb-2 mt-2">
                 {/*info and select*/}
@@ -84,61 +97,83 @@ const ShoppingCartItem = (props: ShoppingCartItem) => {
                             </ul>
                         </div>
                     </div>
-                {/*select*/}
-                <div className="flex w-50 grow md:w-1/2 flex-row gap-2 md:gap-10">
-                    {/*size */}
-                    <select value={size.text} onChange={(e) => {
-                        const selectedSize = sizes.find(s => s.text === e.target.value)
-                        if (selectedSize) {
-                            setSize(selectedSize)
-                        }}
-                    } className="py-2 md:p-2 border-0 rounded-xl text-medium text-[#380557] h-fit bg-csg-violet-100 w-1/4 flex grow md:w-1/6 uppercase truncate text-xs md:text-sm focus:outline-none border-r-4 border-transparent">
-                        {sizes.map((size) => (
-                            <option key={size.text} value={size.text}>
-                                {size.text}
-                            </option>
-                        ))}
-                    </select>
-                    {/*color*/}
-                    <select value={color.name} onChange={(e) => {
-                        const selectedColor = colors.find(c => c.name === e.target.value)
-                        if (selectedColor) {
-                            setColor(selectedColor)
-                        }
-                        }} className="py-2 md:p-2 border-0 rounded-xl text-[#7f2307] h-fit bg-[#ee6c45] w-1/4 md:w-1/4 truncate flex grow uppercase truncate text-xs md:text-sm focus:outline-none border-r-4 border-transparent">
-                        {colors.map((color) => (
-                            <option key={color.name} value={color.name}>
-                                {color.name}
-                            </option>
-                        ))}
-                    </select>
-                    {/*quantity*/}
-                    <div className="w-1/6 flex items-center h-fit">
-                        <button
+                    {/*select*/}
+                    <div className="flex w-50 grow md:w-1/2 flex-row gap-2 md:gap-10">
+                        {/*size */}
+                        <select
+                            value={size.text}
+                            onChange={(e) => {
+                                const selectedSize = sizes.find(
+                                    (s) => s.text === e.target.value
+                                )
+                                if (selectedSize) {
+                                    setSize(selectedSize)
+                                }
+                            }}
+                            className="py-2 md:p-2 border-0 rounded-xl text-medium text-[#380557] h-fit bg-csg-violet-100 w-1/4 flex grow md:w-1/6 uppercase truncate text-xs md:text-sm focus:outline-none border-r-4 border-transparent"
+                        >
+                            {sizes.map((size) => (
+                                <option key={size.text} value={size.text}>
+                                    {size.text}
+                                </option>
+                            ))}
+                        </select>
+                        {/*color*/}
+                        <select
+                            value={color.name}
+                            onChange={(e) => {
+                                const selectedColor = colors.find(
+                                    (c) => c.name === e.target.value
+                                )
+                                if (selectedColor) {
+                                    setColor(selectedColor)
+                                }
+                            }}
+                            className="py-2 md:p-2 border-0 rounded-xl text-[#7f2307] h-fit bg-[#ee6c45] w-1/4 md:w-1/4 truncate flex grow uppercase truncate text-xs md:text-sm focus:outline-none border-r-4 border-transparent"
+                        >
+                            {colors.map((color) => (
+                                <option key={color.name} value={color.name}>
+                                    {color.name}
+                                </option>
+                            ))}
+                        </select>
+                        {/*quantity*/}
+                        <div className="w-1/6 flex items-center h-fit">
+                            <button
                                 className="-mr-2 pr-3 px-2 py-2 rounded-l-xl hover:bg-gray-300 bg-[#45ae95] z-0 text-[#1d594b] text-xs md:text-sm"
-                            onClick={handleDecrement}
-                        >
-                            -
-                        </button>
-                        <input
-                            type="string"
-                            value={Quantity}
-                            onChange={(e) => setQuantity(Math.max(minQuantity, Math.min(props.maxQuantity, Number(e.target.value))))}
+                                onClick={handleDecrement}
+                            >
+                                -
+                            </button>
+                            <input
+                                type="string"
+                                value={Quantity}
+                                onChange={(e) =>
+                                    setQuantity(
+                                        Math.max(
+                                            minQuantity,
+                                            Math.min(
+                                                props.maxQuantity,
+                                                Number(e.target.value)
+                                            )
+                                        )
+                                    )
+                                }
                                 className="w-6 md:w-8 h-fit py-1 text-center rounded-lg bg-[#288e76] z-10 text-[#8ee6d1] text-xs md:text-sm pt-2 pb-2 shrink"
-                        />
-                        <button
+                            />
+                            <button
                                 className="-ml-2 px-2 py-2 pl-3 bg-[#45ae95] rounded-r-xl hover:bg-gray-300 z-0 text-[#1d594b] text-xs md:text-sm"
-                            onClick={handleIncrement}
-                        >
-                            +
-                        </button>
-                    </div>
+                                onClick={handleIncrement}
+                            >
+                                +
+                            </button>
+                        </div>
                     </div>
                 </div>
                 <button className="text-2xl text-center bold ml-auto">
                     <BsXLg />
                 </button>
-        </div>
+            </div>
         </div>
     )
 }
--- components/merch/modals/ShoppingCartModal.tsx
+++ components/merch/modals/ShoppingCartModal.tsx
@@ -1,16 +1,14 @@
 ﻿import React, { useState } from 'react'
-import ShoppingCartItem from "./ShoppingCartItem"
+import ShoppingCartItem from './ShoppingCartItem'
 import { FaLongArrowAltLeft } from 'react-icons/fa'
-import TheButton from "../../generics/TheButton"
+import TheButton from '../../generics/TheButton'
 import { shoppingCartItems } from '@/constants/merch/shoppingCart'
 import { Dispatch, SetStateAction } from 'react'
-
 
 interface PropsInterface {
     toggleModal: Dispatch<SetStateAction<boolean>>
     allChecked: boolean
 }
-
 
 const ShoppingCartModal = (props: PropsInterface) => {
     const { toggleModal, allChecked } = props
@@ -32,13 +30,17 @@ const ShoppingCartModal = (props: PropsInterface) => {
         setCartItems((prevItems) => {
             const updatedItems = prevItems.map((item, i) => {
                 if (i === index) {
-                    const priceChange = item.merch.price * item.quantity;
-                    setTotalPrice((prevTotal) => isCheckedOut ? prevTotal + priceChange : prevTotal - priceChange);
-                    return { ...item, isCheckedOut };
+                    const priceChange = item.merch.price * item.quantity
+                    setTotalPrice((prevTotal) =>
+                        isCheckedOut
+                            ? prevTotal + priceChange
+                            : prevTotal - priceChange
+                    )
+                    return { ...item, isCheckedOut }
                 }
-                return item;
+                return item
             })
-            return updatedItems;
+            return updatedItems
         })
     }
 
@@ -46,7 +48,9 @@ const ShoppingCartModal = (props: PropsInterface) => {
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
             <div className="flex flex-col bg-main-dark text-white p-4 xs:p-6 rounded-2xl max-w-3xl w-full relative border-csg-blue-400 border-2 right-0.5 overflow-hidden">
                 <div className="flex flex-row justify-between gap-2 mb-2">
-                    <div className="text-white sm:text-2xl xl:text-4xl uppercase self-center font-bold font-vietnam tracking-widest">MY SHOPPING BAG</div>
+                    <div className="text-white sm:text-2xl xl:text-4xl uppercase self-center font-bold font-vietnam tracking-widest">
+                        MY SHOPPING BAG
+                    </div>
                     <TheButton onClick={handleCloseModal}>
                         <p className="max-sm:hidden font-semibold w-full h-full flex items-center justify-center gap-x-2">
                             <FaLongArrowAltLeft />
@@ -54,8 +58,10 @@ const ShoppingCartModal = (props: PropsInterface) => {
                         </p>
                     </TheButton>
                 </div>
-                <div className="hidden md:flex w-full h-fit flex-row justify- gap-4 rounded-2xl text-center text-[13px] sm:text-xs md:text-sm bg-black text-[#434365] font-medium font-vietnam tracking-wider">
-                    <div className="w-2/5 ml-16 truncate whitespace-nowrap">PRODUCT DETAILS</div>
+                <div className="hidden md:flex w-full h-fit flex-row gap-4 rounded-2xl text-center text-[13px] sm:text-xs md:text-sm bg-black text-[#434365] font-medium font-vietnam tracking-wider">
+                    <div className="w-2/5 ml-16 truncate whitespace-nowrap">
+                        PRODUCT DETAILS
+                    </div>
                     <div className="w-1/4">SIZE</div>
                     <div className="w-1/4">COLOR</div>
                     <div className="w-1/4">QUANTITY</div>
@@ -72,18 +78,29 @@ const ShoppingCartModal = (props: PropsInterface) => {
                             size={merch.size}
                             maxQuantity={merch.maxQuantity}
                             isCheckedOut={merch.isCheckedOut}
-                            onCheckoutChange={(itemIndex, newCheckedState) => handleCheckoutChange(itemIndex, newCheckedState) }
+                            onCheckoutChange={(itemIndex, newCheckedState) =>
+                                handleCheckoutChange(itemIndex, newCheckedState)
+                            }
                         />
                     ))}
                 </div>
                 <div className="absolute flex flex-row h-fit w-full bg-black bottom-0 left-0 right-0 p-0 m-0 text-center justify-center align-center z-30 text-xs md:text-sm">
-                    <div className={`w-[16px] ps:w-[24px] md:w-[35px] aspect-square rounded-full border-2 md:border-4 border-[#171745] flex shrink-0 cursor-pointer items-center justify-center ${allChecked ? 'bg-[#2929b7]' : 'bg-[#2929b7]/0'
-                        }`} />
+                    <div
+                        className={`w-[16px] ps:w-[24px] md:w-[35px] aspect-square rounded-full border-2 md:border-4 border-[#171745] flex shrink-0 cursor-pointer items-center justify-center ${
+                            allChecked ? 'bg-[#2929b7]' : 'bg-[#2929b7]/0'
+                        }`}
+                    />
                     <p className="basis-1/4 self-center">ALL</p>
                     <div className="basis-1/4 self-center">
-                        <span className="text-[#242460] font-bold">TOTAL</span> PHP { totalPrice }
+                        <span className="text-[#242460] font-bold">TOTAL</span>{' '}
+                        PHP {totalPrice}
                     </div>
-                    <button className="ml-auto bg-[#b53629] pl-2 pr-2" onClick={handleCheckout}>CHECKOUT</button> 
+                    <button
+                        className="ml-auto bg-[#b53629] pl-2 pr-2"
+                        onClick={handleCheckout}
+                    >
+                        CHECKOUT
+                    </button>
                 </div>
             </div>
         </div>
@@ -91,5 +108,3 @@ const ShoppingCartModal = (props: PropsInterface) => {
 }
 
 export default ShoppingCartModal
-
-    //< button onClick = { handleClose } className = "bg-csg-green-100 h-fit px-1 py-0.5 sm:px-2 sm:py-1 rounded-lg flex flex-row gap-0.5 sm:gap-1 items-center cursor-pointer hover:text-white/75" > {< BsArrowLeft />} CONTINUE SHOPPING</button >
