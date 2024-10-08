761b1c654b7b5436c42de33a6d2e4384d522760e
Pixelazation <40255979+Pixelazation@users.noreply.github.com>
2024-10-08 12:36:16 +0800

    [Misc][Joshua] Added mock data for content list

---
+++ constants/admin/articles.ts
@@ -0,0 +1,29 @@
+export const articles = [
+    {
+        title: "string",
+        image: "/images/placeholder.png",
+        date: new Date("2024-01-07"),
+        body: "Very short body",
+        author: {
+            name: "Bazer Nuñez"
+        },
+    },
+    {
+        title: "string",
+        image: "/images/placeholder.png",
+        date: new Date("2024-01-07"),
+        body: "Very short body",
+        author: {
+            name: "Bazer Nuñez"
+        },
+    },
+    {
+        title: "string",
+        image: "/images/placeholder.png",
+        date: new Date("2024-01-07"),
+        body: "Very short body",
+        author: {
+            name: "Bazer Nuñez"
+        },
+    },
+]
