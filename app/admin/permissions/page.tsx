54abc2d411b965b27c3636cf99ce3f3ed9b57316
leidc024 <lidelacruz1@up.edu.ph>
2024-10-09 22:06:01 +0800

    [Feature][Lei] added UI/Pagination to Permission List

--- app/admin/permissions/page.tsx
+++ app/admin/permissions/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 
 const AdminPermissions = () => {
@@ -5,3 +6,30 @@ const AdminPermissions = () => {
 }
 
 export default AdminPermissions
+=======
+﻿'use client';
+
+import React, { useState } from 'react';
+import Header from './Header';
+import Table from './TableSection';
+import Page from './Pagination';
+
+const AdminPermissions = () => {
+    const [currentPage, setCurrentPage] = useState(1); // Hold the current page state
+
+    return (
+        <div className="p-6 text-white mx-auto w-4/5 bg-[#0a0e22]">
+            {/* Header Section */}
+            <Header />
+
+            {/* Table Section */}
+            <Table currentPage={currentPage} />
+
+            {/* Pagination */}
+            <Page currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        </div>
+    );
+};
+
+export default AdminPermissions;
+>>>>>>> Stashed changes
