03a4043931a4d31ce1823c4d7f48970b79d89460
rokkunbruv <themorghiknight579@gmail.com>
2024-10-15 23:28:59 +0800

    [Feature][Christian] moved state handling to page route

--- app/admin/create/content/page.tsx
+++ app/admin/create/content/page.tsx
@@ -3,12 +3,33 @@
 import React from 'react'
 
 import CreateEventForm from '@/components/admin/create/content/CreateEventForm'
+import useFormHandler from '@/hooks/FormHooks'
 
 const AdminCreateContent = () => {
+    // initial values for form data
+    const initialValues = {
+        eventTitle: '',
+        startTime: '',
+        endTime: '',
+        eventLocation: '',
+        eventDay: '',
+        eventMonth: '',
+        eventYear: '',
+        image: null,
+    }
+
+    // create event form hooks
+    const { formData, handleChange, handleImageChange, handleSubmit } =
+        useFormHandler(initialValues)
+
     return (
         <section className="px-14 md:px-32">
             {/* Create Event Selector */}
-            <CreateEventForm />
+            <CreateEventForm
+                formData={formData}
+                handleChange={handleChange}
+                handleImageChange={handleImageChange}
+            />
         </section>
     )
 }
