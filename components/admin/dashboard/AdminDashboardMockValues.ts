f77d41867e079a3ec9bfba69e3a60140a8fcbfd1
Az <arbusinos@up.edu.ph>
2024-10-17 22:17:34 +0800

    [chore][az] added mock values for cms dashboard summary

---
+++ components/admin/dashboard/AdminDashboardMockValues.ts
@@ -0,0 +1,36 @@
+export const dashboardData = [
+    {
+        count: 3,
+        countDescription: 'news &\nevents',
+        recentTitle: 'AI Pilipinas',
+        recentAuthor: 'Maxell Milay',
+        cardTitle: 'articles',
+        cardTitleBackgroundColor: '#3e2f5d',
+        cardBodyBackgroundColor: '#47366a',
+    },
+    {
+        count: 0,
+        countDescription: 'ongoing\napps.',
+        recentTitle: 'DOST JLSS 2025',
+        recentAuthor: 'DOST',
+        cardTitle: 'scholarships',
+        cardTitleBackgroundColor: '#4d4da5',
+        cardBodyBackgroundColor: '#5858af',
+    },
+    {
+        count: 10,
+        countDescription: 'open for\nhiring',
+        recentTitle: 'Software Dev Intern',
+        recentAuthor: 'Milay Labs',
+        cardTitle: 'internships',
+        cardTitleBackgroundColor: '#4f76c3',
+        cardBodyBackgroundColor: '#738bd0',
+    },
+]
+
+export const latestAnnouncement = {
+    imgPath: '/title-header.png',
+    title: 'DOST Scholarship Program | Now Accepting Applicants 2024',
+    description: `Allowance: ₱40,000 and other benefits\nGrade Req.: GWA of 83% or its equivalent`,
+    date: 'September 5, 2023',
+}
