5c7029ae3506b3b254ec68a7f208243f6265fe9e
Maxell Milay <milaymaxell@gmail.com>
2025-04-21 14:52:52 +0800

    setup state/data management and user authentication in FE

---
+++ interface/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    email: string;
+    last_login: string;
+    is_superuser: string;
+    first_name: string;
+    last_name: string;
+    is_staff: boolean;
+    is_active: boolean;
+    date_joined: string;
+    avatar_url: string;
+    is_host: boolean;
+}
