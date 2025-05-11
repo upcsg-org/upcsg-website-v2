5e02310f0a696e053f11d873b99cec944ab32b25
Loweso <dlouisefermin@gmail.com>
2025-05-11 20:49:27 +0800

    connected events page with backend

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import ComingSoon from '@/components/generics/ComingSoon'
+import EventPage from '@/components/events/EventPage'
 
 const EventsPage = () => {
     return (
-        <section className="h-[calc(100vh-6rem)]">
-            <ComingSoon />
-        </section>
+        <div>
+            <div
+                className="flex w-full items-center justify-center text-center bg-gradient-to-r
+                            from-csg-blue-600/25 to-csg-blue-600/25 via-csg-blue-600 h-28 sm:h-36 xl:h-56
+                            tracking-widest font-vietnam font-bold text-white text-xl sm:text-4xl xl:text-6xl"
+            >
+                ORGANIZATION
+                <br />
+                EVENTS
+            </div>
+            <EventPage />
+        </div>
     )
 }
 
--- components/events/EventCard.tsx
+++ components/events/EventCard.tsx
@@ -21,12 +21,19 @@ const EventCard = (props: PropsInterface) => {
         image_url,
         title,
         start_date,
+        end_date,
         article,
         external_url,
         location,
     } = props
 
-    const eventPath = article ? `/events/${id}` : (external_url ?? '/')
+    const eventPath = article
+        ? `/events/${id}`
+        : external_url
+          ? external_url
+          : null
+
+    const isClickable = !!eventPath
 
     const neonGradient =
         customNeonGradients[
@@ -35,13 +42,19 @@ const EventCard = (props: PropsInterface) => {
 
     return (
         <motion.button
-            className="relative overflow-hidden rounded-2xl max-md:w-[90%] w-full h-52 md:h-64 text-left bg-cover bg-center"
+            className={`relative overflow-hidden rounded-2xl max-md:w-[90%] w-full h-52 md:h-64 text-left bg-cover bg-center ${
+                isClickable ? 'cursor-pointer' : 'cursor-default'
+            }`}
             style={{
                 backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url('${image_url}')`,
             }}
-            whileHover={{ scale: 1.05 }}
+            whileHover={isClickable ? { scale: 1.05 } : {}}
             transition={{ duration: 0.3 }}
-            onClick={() => (window.location.href = eventPath)}
+            onClick={() => {
+                if (isClickable && eventPath) {
+                    window.location.href = eventPath
+                }
+            }}
         >
             {/* Top black gradient */}
             <div className="absolute top-0 left-0 w-full h-20 bg-gradient-to-b from-black to-transparent z-10" />
@@ -57,25 +70,26 @@ const EventCard = (props: PropsInterface) => {
             {/* Content */}
             <div className="flex flex-col justify-between h-full p-4 relative z-20">
                 <div>
-                    <p className="text-lg font-vietnam font-extrabold text-white">
+                    <p className="text-md md:text-lg font-vietnam font-extrabold text-white">
                         {title}
                     </p>
                 </div>
-                <div className="flex justify-between items-center">
-                    {start_date && (
+                <div className="flex flex-row justify-between items-center">
+                    {start_date && end_date && (
                         <div className="flex items-center">
-                            <FaRegCalendarAlt className="h-4 w-4 text-white" />
+                            <FaRegCalendarAlt className="h-3 w-3 md:h-4 md:w-4 text-white" />
                             <div className="pl-2">
-                                <p className="text-sm font-bold font-vietnam text-white">
-                                    {getDateString(new Date(start_date))}
+                                <p className="text-xs md:text-sm font-bold font-vietnam text-white">
+                                    {getDateString(new Date(start_date))} -{' '}
+                                    {getDateString(new Date(end_date))}
                                 </p>
                             </div>
                         </div>
                     )}
                     {start_date && (
-                        <p className="text-sm font-thin text-white mb-2">
+                        <div className="self-center hidden md:block text-xs md:text-sm font-thin text-white mb-2">
                             {getDayOfWeek(new Date(start_date))}
-                        </p>
+                        </div>
                     )}
                 </div>
             </div>
