57facc1b14c860f39d8a8913ddec60d1c05c1e9c
Maxell Milay <milaymaxell@gmail.com>
2025-05-29 02:44:51 +0800

    updated permissions page

--- app/admin/permissions/page.tsx
+++ app/admin/permissions/page.tsx
@@ -1,13 +1,12 @@
+'use client'
 
-﻿'use client';
-
-import React, { useState } from 'react';
-import Header from './header';
-import Table from './TableSection';
-import Page from './Pagination';
+import React, { useState } from 'react'
+import Header from './header'
+import Table from './TableSection'
+import Page from './Pagination'
 
 const AdminPermissions = () => {
-    const [currentPage, setCurrentPage] = useState(1); // Hold the current page state
+    const [currentPage, setCurrentPage] = useState(1) // Hold the current page state
 
     return (
         <div className="p-6 text-white mx-auto w-4/5 bg-[#0a0e22]">
@@ -20,8 +19,7 @@ const AdminPermissions = () => {
             {/* Pagination */}
             <Page currentPage={currentPage} setCurrentPage={setCurrentPage} />
         </div>
-    );
-};
+    )
+}
 
-export default AdminPermissions;
-
+export default AdminPermissions
